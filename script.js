0fa46c8ada22f77dc81bba313e77d68c766f5cb0
weeer <kzs1845@gmail.com>
2021-07-27 22:08:28 +0300

    final

--- script.js
+++ script.js
@@ -16,13 +16,23 @@ function showSuccess(input) {
 	formControl.className = 'form-control success';
 }
 
-function isValidEmail(email) {
+function checkEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     // метод тест выполняет поиск сопоставления регулярного выражения
     // указанной строке, возвращает тру или фолз
     // ре - наша эталонная строка и мы передаем в ее тест строку из инпута
     // значение ре взято со стакОверфлоу
-    return re.test(String(email).toLowerCase());	
+    if (re.test(email.value.trim())) {
+    	showSuccess(email) 
+    } else {
+    	showError(email, 'Вы ввели почту неправильно');
+    }
+}
+
+function checkPasswordsMatch(input, input2) {
+	if (input.value !== input2.value) {
+		showError(input2, 'Пароли не совпадают')
+	}
 }
 
 function getFieldName(input) {
@@ -42,8 +52,20 @@ function checkRequired(inputArray) {
 	})
 }
 
+function checkLength(input, min, max) {
+	if (input.value.length < min) {
+		showError(input, `${getFieldName(input)} должно быть хотя бы ${min} букв`)
+	} else if (input.value.length > max) {
+		showError(input, `${getFieldName(input)} должно быть меньше ${max} букв`)
+	}
+}
+
 form.addEventListener('submit', (evt) => {
 	evt.preventDefault();
 
 	checkRequired([username, email, password, password2]);	
+	checkLength(username, 3, 15);
+	checkLength(password, 6, 25);
+	checkEmail(email);
+	checkPasswordsMatch(password, password2)
 })
